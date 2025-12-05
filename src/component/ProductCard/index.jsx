fdf3f5c16eece45c3bc66872584d9ef6ec1748eb
rose <rose.dangol23@gmail.com>
2025-12-05 16:43:50 +0545

    Added some custom Tailwind theme configuration

--- src/component/ProductCard/index.jsx
+++ src/component/ProductCard/index.jsx
@@ -3,37 +3,39 @@ import React from "react";
 const ProductCard = ({products}) => {
   return (
     <div className="h-auto w-full mx-auto p-5">
-      <div className=" h-auto flex flex-col justify-center items-center p-10">
+      <div className="container mx-auto p-5 mb-3.5 font-sans">
         <div className="flex flex-col gap-3 mb-5 justify-center items-center text-center">
-          <span className="text-[#737373] text-lg hidden md:inline">Featured Products</span>
+          <span className="text-[#737373] text-lg hidden md:inline font-sans">Featured Products</span>
           <span className="text-2xl font-semibold leading-none">
             BESTSELLER PRODUCTS
           </span>
           <span className="text-[#737373] text-md">sth sth</span>
         </div>
-        <div className="flex flex-wrap gap-3 justify-center mb-3">
+        <div className="mx-auto grid grid-cols-5 gap-x-6 gap-y-3 p-5 w-[80%]">
           {products.map((product) => (
             <div
-              className="flex flex-col gap-2 justify-center items-center border-[#EDEDED] border-2 pb-3"
+              className="h-[400px] w-[183px] flex flex-col gap-2 items-center border-[#EDEDED] pb-3"
               key={product.id}
             >
-              <div className="h-100 w-80">
+              <div className="h-[238px] ">
                 <img
                   src={product.image}
                   className="h-full w-full object-cover"
                 />
               </div>
-              <span className="text-base font-bold">{product.title}</span>
-              <span className="text-sm font-semibold">
-                {product.description}
-              </span>
-              <span className="text-base font-semibold text-[#23856D]">
-                ${product.price}
-              </span>
+              <div className="flex flex-col p-[25px] pb-[35px] items-center justify-center gap-2.5 text-primary">
+                <span className="text-base font-bold text-center">{product.title}</span>
+                <span className="text-xs font-bold text-center text-[#737373] mb-1">
+                  {product.description}
+                </span>
+                <span className="text-base font-semibold text-[#23856D]">
+                  ${product.price}
+                </span>
+              </div>
             </div>
           ))}
         </div>
-        <button className="border-2 border-[#23A6F0] rounded-md px-8 py-3 text-[#23A6F0] font-semibold mt-5">
+        <button className="mx-auto block border-2 border-primary rounded-md px-8 py-3 text-primary font-semibold mt-5 hover:bg-primary hover:text-white hover:cursor-pointer">
           LOAD MORE PRODUCTS
         </button>
       </div>
--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,65 +1,93 @@
 import React from "react";
-import {TopDetail, Navbar, BrandLogos, ProductCard, Footer } from "../../component/index";
+import {
+  TopDetail,
+  Navbar,
+  BrandLogos,
+  ProductCard,
+  Footer,
+} from "../../component/index";
 const Landing = () => {
   const products = [
     {
       id: 1,
       image: "/images/product1.jpg",
-      title: "Adidas Originals",
-      description: "6 pack socks in brown",
+      title: "Graphic Design",
+      description: "English Department",
       price: 29.99,
     },
     {
       id: 2,
-      image: "/featured-products/Spezial.jpg",
-      title: "Adidas Originals Spezial",
-      description: "Handball gum sole trainers in brown and white",
+      image: "/images/product2.jpg",
+      title: "Graphic Design",
+      description: "English Department",
       price: 49.99,
     },
     {
       id: 3,
-      image: "/featured-products/Adicolor.jpg",
-      title: "Adicolor knitted jersey",
-      description: "adidas Originals in white",
+      image: "/images/product3.jpg",
+      title: "Graphic Design",
+      description: "English Department",
       price: 49.99,
     },
     {
       id: 4,
-      image: "/featured-products/addidas-socks.jpg",
-      title: "Product 4",
-      description: "Description 2",
+      image: "/images/product4.jpg",
+      title: "Graphic Design",
+      description: "English Department",
       price: 49.99,
     },
     {
       id: 5,
-      image: "/featured-products/TrackPant.jpg",
-      title: "Firebird",
-      description: "adidas Originals track pants in brown",
+      image: "/images/product5.jpg",
+      title: "Graphic Design",
+      description: "English Department",
       price: 49.99,
     },
     {
       id: 6,
-      image: "/featured-products/TrackTop.jpg",
-      title: "Firebird Fluffy",
-      description: "adidas Originals track top in burgundy",
+      image: "/images/product1.jpg",
+      title: "Graphic Design",
+      description: "English Department",
       price: 49.99,
     },
     {
       id: 7,
-      image: "/featured-products/jacket.jpg",
-      title: "The Item",
-      description: "WOOL BLEND JACKET",
+      image: "/images/product6.jpg",
+      title: "Graphic Design",
+      description: "English Department",
+      price: 129.0,
+    },
+    {
+      id: 8,
+      image: "/images/product7.jpg",
+      title: "Graphic Design",
+      description: "English Department",
+      price: 129.0,
+    },
+    {
+      id: 9,
+      image: "/images/product8.jpg",
+      title: "Graphic Design",
+      description: "English Department",
+      price: 129.0,
+    },
+    {
+      id: 10,
+      image: "/images/product9.jpg",
+      title: "Graphic Design",
+      description: "English Department",
       price: 129.0,
     },
   ];
 
   return (
     <div className="flex flex-col flex-1 w-full">
-      <TopDetail/>
-      <Navbar/>
+      <TopDetail />
+      <Navbar />
+
       {/* <Banner/> gradient left to right 97e9fa 99e9f6 a2ebe6 abecd7 */}
-      <div className="py-10 w-full md:pl-10 md:pr-15 p-4">
-        <div className="rounded-3xl bg-linear-to-r from-[#97e9fa] to-[#abecd7] w-full items-center flex flex-col gap-5 md:flex-row justify-end">
+      <div className="py-10 w-full mx-auto md:pl-10 md:pr-15 p-4">
+        <div className="rounded-3xl bg-linear-to-r from-[#97e9fa] to-[#abecd7] w-19/20 items-center flex flex-col gap-5 md:flex-row justify-end h-[620px] mx-auto">
           {/* banner texts*/}
           <div className="flex flex-col justify-center items-center md:justify-around md:items-start pt-10 gap-4 md:gap-6 md:w-1/2 md:pl-25">
             <span className="text-sm font-bold text-[#2A7CC7]">
@@ -76,14 +104,30 @@ const Landing = () => {
             </button>
           </div>
           {/* banner image */}
-          <div className="md:w-1/2 w-full h-full">
+          <div className="md:w-1/2 w-full h-full justify-items-end relative">
+            <img src="/images/Ellipse-bg.png" alt="bg-ellipse" className="" />
             <img
-              src="/images/banner-image.png"
+              src="/images/Ellipse-midSize-top.png"
               alt=""
-              className="w-full h-full object-cover"
+              className="absolute top-0 left-15"
             />
+            <img
+              src="/images/bannerImage-girl.png"
+              alt="bg-ellipse"
+              className="absolute bottom-0 -right-20"
+            />
+            <img
+              src="/images/Ellipse-purple-right.png"
+              alt="bg-ellipse"
+              className="absolute -right-18 top-1/5"
+            />
+            <img
+              src="/images/Ellipse-purple-left.png"
+              alt="bg-ellipse"
+              className="absolute left-[25%] bottom-[30%]"
+            />
+            {/* <img src="/images/banner-image.png" alt="" className="w-full h-full object-cover"/> */}
           </div>
-
         </div>
       </div>
 
@@ -91,98 +135,100 @@ const Landing = () => {
       <BrandLogos />
 
       {/*top products of the week section */}
-<div className="hidden">
-      <div className="p-3 flex justify-center h-auto">
-        <div className="flex gap-3 w-4/5 h-140">
-          {/* left */}
-          <div className="w-1/2 h-full relative">
-            <img
-              src="/images/top-week1.png"
-              alt=""
-              className="w-full h-full object-cover"
-            />
-            {/* eslai ma component banauchu and left ho ki right props thru send garchu? and tei anusar width and height change huncha? idk */}
-            <div className="absolute bottom-0 left-0 bg-[#23A6F0]/60 p-5 w-7/10 h-2/5">
-              <div className="text-white flex flex-col p-5 gap-3 items-start justify-center h-full">
-                <span className="text-3xl font-bold tracking-wider">
-                  Top Product of the Week
-                </span>
-                <button className="border-2 border-amber-50 px-5 py-4 w-3/5">
-                  EXPLORE ITEMS
-                </button>
+      <div className="">
+        <div className="p-3 flex justify-center h-auto">
+          <div className="flex gap-3 w-4/5 h-140">
+            {/* left */}
+            <div className="w-1/2 h-full relative">
+              <img
+                src="/images/top-week1.png"
+                alt=""
+                className="w-full h-full object-cover"
+              />
+              {/* eslai ma component banauchu and left ho ki right props thru send garchu? and tei anusar width and height change huncha? idk */}
+              <div className="absolute bottom-0 left-0 bg-primary/60 p-5 w-7/10 h-2/5">
+                <div className="text-white flex flex-col p-5 gap-3 items-start justify-center h-full">
+                  <span className="text-3xl font-bold tracking-wider">
+                    Top Product of the Week
+                  </span>
+                  <button className="border-2 border-amber-50 px-5 py-4 w-3/5">
+                    EXPLORE ITEMS
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* right */}
+            <div className="flex flex-col w-1/2 h-full">
+              <div className="h-1/2 relative">
+                <img
+                  src="/images/top-week2.png"
+                  alt=""
+                  className="w-full h-full object-cover"
+                />
+                <div className="absolute bottom-0 left-0">View Now</div>
+              </div>
+              <div className="h-1/2 relative">
+                <img
+                  src="/images/top-week3.png"
+                  alt=""
+                  className="w-full h-full object-cover"
+                />
+                <div className="absolute bottom-0 left-0">View Now</div>
               </div>
             </div>
           </div>
-          {/* right */}
-          <div className="flex flex-col w-1/2 h-full gap-2">
-            <div className="h-1/2 relative">
-              <img
-                src="/images/top-week2.png"
-                alt=""
-                className="w-full h-full object-cover"
-              />
-              <div className="absolute bottom-0 left-0">View Now</div>
-            </div>
-            <div className="h-1/2 relative">
-              <img
-                src="/images/top-week3.png"
-                alt=""
-                className="w-full h-full object-cover"
-              />
-              <div className="absolute bottom-0 left-0">View Now</div>
-            </div>
-          </div>
         </div>
       </div>
-</div>
 
       {/* featured products */}
-        <ProductCard products={products}/>
-<div className="hidden">
-      {/* about us sort of? ig */}
-      <div className=" h-auto w-full mb-5">
-        <div className="w-3/4  mx-auto flex items-center justify-items-start gap-5">
-          <div className="flex gap-4">
-            <div className="h-125 w-2/5">
-              <img
-                src="images/top-product-bg-1.jpg"
-                alt=""
-                className="h-full w-full object-cover"
-              />
+      <ProductCard products={products}/>
+
+
+      <div className="">
+        {/* about us sort of? ig */}
+        <div className=" h-auto w-full mb-5 border-amber-300 border-2">
+          <div className="w-3/4  mx-auto flex items-center justify-items-start gap-5">
+            <div className="flex gap-4">
+              <div className="h-125 w-2/5">
+                <img
+                  src="images/about-left.jpg"
+                  alt=""
+                  className="h-full w-full object-cover"
+                />
+              </div>
+              <div className="h-125 w-1/2">
+                <img
+                  src="images/about-right.jpg"
+                  alt=""
+                  className="h-full w-full object-cover"
+                />
+              </div>
             </div>
-            <div className="h-125 w-1/2">
-              <img
-                src="images/top-product-bg-3.jpg"
-                alt=""
-                className="h-full w-full object-cover"
-              />
+            <div className="flex flex-col gap-3 w-1/2">
+              <span className="text-[#23a6f0] font-semibold text-sm tracking-tight">
+                About US
+              </span>
+              <span className="font-bold text-5xl tracking-wide text-[#252B42]">
+                We love what we do
+              </span>
+              <span className="text-[#727272]">
+                White Feather's Jewellery prioritizes security, ensuring that
+                every transaction conducted on the site is secure and seamless
+                for clients. To do this, the organization follows to strict
+                transparency regulations throughout the entire consumer
+                purchasing process. To add to the convenience, all White
+                Feather's Jewellery goods has life time exchanging facilities
+                and 15 days return policy
+              </span>
             </div>
-          </div>
-          <div className="flex flex-col gap-3 w-1/2">
-            <span className="text-[#23a6f0] font-semibold text-sm tracking-tight">
-              About US
-            </span>
-            <span className="font-bold text-5xl tracking-wide text-[#252B42]">
-              We love what we do
-            </span>
-            <span className="text-[#727272]">
-              White Feather's Jewellery prioritizes security, ensuring that
-              every transaction conducted on the site is secure and seamless for
-              clients. To do this, the organization follows to strict
-              transparency regulations throughout the entire consumer purchasing
-              process. To add to the convenience, all White Feather's Jewellery
-              goods has life time exchanging facilities and 15 days return
-              policy
-            </span>
           </div>
         </div>
+        {/* best services */}
+
+        {/* arko featured posts */}
+
+        <Footer />
       </div>
-      {/* best services */}
-
-      {/* arko featured posts */}
-
-      <Footer/>
-</div>
     </div>
   );
 };
